af1cd98b487923d3f8e6592312a70c49a773fddd
Casey Patrick Driscoll <caseypatrickdriscoll@me.com>
2015-01-18 19:42:56 -0500

    Init commit, for real

---
+++ idk-dashboard.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Plugin Name: iDK Dashboard
+ * Plugin URI:
+ * Description: The central dashboard for the iDK
+ * Author: Casey Patrick Driscoll
+ * Author URI: http://caseypatrickdriscoll.com
+ */
+
+class iDK_Dashboard {
+
+
+	function __construct() {
+
+	}
+
+}
+
+new iDK_Dashboard();
